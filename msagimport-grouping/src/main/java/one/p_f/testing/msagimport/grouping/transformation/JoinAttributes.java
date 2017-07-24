10c5e1e17474147c40e21ea9dd72c14fc7ba430b
Johannes Leupold <johannes.leupold@schie-le.de>
2017-07-24 17:27:03 +0200

    Add Aggregator and Transformation for attribute grouping. [not tested]
    Signed-off-by: Johannes Leupold <johannes.leupold@schie-le.de>

---
+++ msagimport-grouping/src/main/java/one/p_f/testing/msagimport/grouping/transformation/JoinAttributes.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 TraderJoe95 <johannes.leupold@schie-le.de>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.p_f.testing.msagimport.grouping.transformation;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import org.gradoop.common.model.impl.pojo.Edge;
+import org.gradoop.common.model.impl.pojo.Vertex;
+import org.gradoop.flink.model.api.functions.TransformationFunction;
+import org.gradoop.flink.model.impl.LogicalGraph;
+import org.gradoop.flink.model.impl.operators.transformation.Transformation;
+
+/**
+ *
+ * @author TraderJoe95
+ */
+public class JoinAttributes {
+
+    private Transformation trans;
+
+    public JoinAttributes() {
+        TransformationFunction<Vertex> vertexFunc = (c, t) -> {
+            Iterable<String> keys = c.getPropertyKeys();
+            String joined = StreamSupport.stream(keys.spliterator(), false)
+                    .collect(Collectors.joining(";"));
+
+            t.getPropertyKeys().forEach(key -> t.getProperties().remove(key));
+            t.setProperty("attributes", joined);
+
+            return t;
+        };
+
+        TransformationFunction<Edge> edgeFunc = (c, t) -> {
+            Iterable<String> keys = c.getPropertyKeys();
+            String joined = StreamSupport.stream(keys.spliterator(), false)
+                    .collect(Collectors.joining(";"));
+
+            t.getPropertyKeys().forEach(key -> t.getProperties().remove(key));
+            t.setProperty("attributes", joined);
+
+            return t;
+        };
+
+        trans = new Transformation(null, vertexFunc, edgeFunc);
+    }
+
+    public LogicalGraph execute(LogicalGraph graph) {
+        return trans.execute(graph);
+    }
+}
---
+++ msagimport-grouping/src/main/java/one/p_f/testing/msagimport/grouping/transformation/SplitAttributes.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017 TraderJoe95 <johannes.leupold@schie-le.de>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.p_f.testing.msagimport.grouping.transformation;
+
+import java.util.Arrays;
+import org.gradoop.common.model.impl.pojo.Edge;
+import org.gradoop.common.model.impl.pojo.Vertex;
+import org.gradoop.flink.model.api.functions.TransformationFunction;
+import org.gradoop.flink.model.impl.LogicalGraph;
+import org.gradoop.flink.model.impl.operators.transformation.Transformation;
+
+/**
+ *
+ * @author TraderJoe95
+ */
+public class SplitAttributes {
+
+    private Transformation trans;
+
+    public SplitAttributes() {
+        TransformationFunction<Vertex> vertexFunc = (c, t) -> {
+            String joined = c.getProperties().get("attributes").getString();
+
+            t.getPropertyKeys().forEach(key -> t.getProperties().remove(key));
+            Arrays.stream(joined.split(";")).peek(System.out::println)
+                    .forEach(key -> t.setProperty(key, "value"));
+
+            return t;
+        };
+
+        TransformationFunction<Edge> edgeFunc = (c, t) -> {
+            String joined = c.getProperties().get("attributes").getString();
+
+            t.getPropertyKeys().forEach(key -> t.getProperties().remove(key));
+            Arrays.stream(joined.split(";")).peek(System.out::println)
+                    .forEach(key -> t.setProperty(key, "value"));
+
+            return t;
+        };
+
+        trans = new Transformation(null, vertexFunc, edgeFunc);
+    }
+
+    public LogicalGraph execute(LogicalGraph graph) {
+        return trans.execute(graph);
+    }
+}
